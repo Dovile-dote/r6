8492e193285879c88e9d181765648f4c19f5d954
Dovile-dote <80914644+Dovile-dote@users.noreply.github.com>
2022-06-02 20:54:14 +0300

    pradzia

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import './style.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>sass</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
